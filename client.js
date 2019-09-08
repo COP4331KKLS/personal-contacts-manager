36bf99101ecc744e90dbafb61a89ff5509cb76e3
Stefan Werleman <stefanwerleman@knights.ucf.edu>
2019-09-08 18:16:04 -0400

    added a contacts page and a cancel button to each page

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const Http = new XMLHttpRequest();
-const url = 'https://personal-contacts-manager.herokuapp.com';
+const url = 'https://personal-contacts-manager.herokuapp.com/contacts';
 
 Http.open("GET", url);
 Http.send;
@@ -8,3 +8,18 @@ Http.onreadystatechange = (e) =>
 {
 	console.log(Http.responseText)
 }
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("POST", '/user', true);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.onreadystagechange = function()
+{
+	if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
+	{
+
+	}
+}
+
+xhr.send("name=Ipseeta&id=1");
