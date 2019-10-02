d147e83892585560f1d77f2254fc9088126c5f41
KyleTheKracka <53285301+KyleTheKracka@users.noreply.github.com>
2019-10-02 17:43:51 -0400

    Modal pre populates

--- src/EditContact/EditContactModal.js
+++ src/EditContact/EditContactModal.js
@@ -1,51 +1,25 @@
 import React, { Component } from 'react';
 import './EditContactModal.css';
 import { Card, CardBody } from 'reactstrap';
+// import {getFirstName} from '../home/Home'
+
 
 class EditContactModal extends Component
 {
    render()
    {
-      let editContact = (
-         <Card className = "CurrentContactCard">
-            <CardBody>
-               <h4>Edit Contact</h4>
-               <form className = "EditContact">
-                  <label id = "first-name">First Name</label>
-                  <input value="" type = "text" id = "first-name-input" name = "first-name"/>
+      // let editContact = (
+      //
+      // );
 
-                  <label id = "last-name">Last Name</label>
-                  <input value="" type = "text" id = "last-name" name = "last-name"/>
-
-                  <label id = "company">Company</label>
-                  <input value="" type = "text" id = "company" name = "company"/>
-
-                  <label id = "phone-number">Phone Number</label>
-                  <input value="" type = "text" id = "phone-number" name = "phone-number"/>
-
-                  <label id = "email">Email</label>
-                  <input value="" type = "text" id = "email" name = "email"/>
-
-                  <label id = "address">Address</label>
-                  <input value="" type = "text" id = "address" name = "address"/>
-
-                  <br></br>
-
-                  <button class = "EditButton">Edit</button>
-                  <button onClick = {this.props.onClose}>Close</button>
-               </form>
-            </CardBody>
-         </Card>
-      );
-
-      if (!this.props.isEditContactOpen)
-      {
-         editContact = null;
-      }
+      // if (!this.props.isEditContactOpen)
+      // {
+      //    editContact = null;
+      // }
 
       return (
          <div>
-            {editContact}
+            {this.props.children}
          </div>
       );
    }
--- src/home/Home.js
+++ src/home/Home.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import './Home.css';
 import CreateContactModal from '../create-contact/CreateContactModal'
 import EditContactModal from '../EditContact/EditContactModal'
+import { Card, CardBody } from 'reactstrap';
+
 // import ReactDOM from "react-dom"
 // import Modal from 'react-modal'
-import Modal from "../EditContactModal/editContactModal";
+// import Modal from "../EditContactModal/editContactModal";
 class Home extends Component
 {
    state = {
      isCreateContactOpen: false,
      isEditContactOpen: false,
-      isOpen: false
+      isOpen: false,
+      showContactOpen: false
    }
    constructor(props)
    {
@@ -35,27 +38,42 @@ class Home extends Component
                             "Contact ID": "Blah"
                         },
                         {
-                          "First Name": "Kyle",
-                          "Last Name" : "Rits",
-                          "Company" : "UCF",
-                          "Phone" : "808-990-5604",
-                          "Email": "KyleRits@Knights.ucf.edu",
+                          "First Name": "Kevin",
+                          "Last Name" : "Rabinasdfasdfa",
+                          "Company" : "POOP",
+                          "Phone" : "123456789",
+                          "Email": "KFJADASF@Knights.ucf.edu",
                           "Address": "",
-                          "Contact ID": "Test"
+                          "Contact ID": "2"
                         },
                         {
-                          "First Name": "Kyle",
-                          "Last Name" : "Rits",
-                          "Company" : "UCF",
-                          "Phone" : "808-990-5604",
-                          "Email": "KyleRits@Knights.ucf.edu",
-                          "Address": "3721 Pyrite Drive",
-                          "Contact ID": "Yeah"
+                          "First Name": "LLOYD",
+                          "Last Name" : "SOMETHING?",
+                          "Company" : "POOPS",
+                          "Phone" : "987654321",
+                          "Email": "LLOYD@Knights.ucf.edu",
+                          "Address": "NOT HERE",
+                          "Contact ID": "4"
                         }
                     ],
-           doRender: false
+           doRender: false,
+           editPreFillFirstName: "new",
+           editPreFillLastName: "",
+           editPreFillCompany : "",
+           editPreFillPhone: "",
+           editPreFillEmail: "",
+           editPreFillAddress: "",
+           editPreFillContactID: ""
                   }
+
+                  this.getFirstName = this.getFirstName.bind(this);
    }
+
+   getFirstName() {
+     console.log(this.editPreFillFirstName);
+     console.log("^^^");
+     return this.editPreFillFirstName;
+   };
 
    renderTableData() {
     return this.state.contacts.map((contact, index) => {
@@ -87,18 +105,41 @@ class Home extends Component
 
  editContact = (el) =>{
    const First_Name = el.target.getAttribute('fName');
+   const Last_Name = el.target.getAttribute('lName');
+   const Company = el.target.getAttribute('company');
+   const Phone = el.target.getAttribute('phone');
+   const Email = el.target.getAttribute('email');
+   const Address =  el.target.getAttribute('address');
    console.log(First_Name);
+   console.log(Last_Name);
+   console.log(Company);
+   console.log(Phone);
+   console.log(Email);
+   console.log(Address);
+   // this.getFirstName();
    console.log("Edit Contact");
+   // modal.firstname.value =
    // document.getElementById("EditContactss").getElementById("first-name-input");
    // this.editContact.getElementById("first-name-input").value = First_Name;
+   // document.getElementById("first-name-edit").value=First_Name;
    console.log(el.target.value);
+   this.setState({editPreFillFirstName: First_Name});
+   this.setState({editPreFillLastName: Last_Name});
+   this.setState({editPreFillCompany : Company});
+   this.setState({editPreFillPhone: Phone});
+   this.setState({editPreFillEmail: Email});
+   this.setState({editPreFillAddress: Address});
    this.setState({showContactOpen: true});
  }
 
+submitEditContact= (e) =>{
+  this.setState({showContactOpen: false});
+}
 
-   deleteContact(e){
+   deleteContact= (e) =>{
      console.log("Delete Contact");
      console.log(e.target.value);
+     this.setState({doRender: false});
    }
 
    renderTableHeader() {
@@ -108,22 +149,6 @@ class Home extends Component
          return <th key={index}>{key.toUpperCase()}</th>
       })
    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
    render()
    {
@@ -142,7 +167,40 @@ class Home extends Component
 
                <button className = "NewContactButton" onClick = {(e) => this.setState({ isCreateContactOpen: true })}>Add New Contact</button>
                <CreateContactModal isCreateContactOpen = {this.state.isCreateContactOpen} onClose = {(e) => this.setState({isCreateContactOpen: false})}/>
-               <EditContactModal id="EditContactss" isEditContactOpen = {this.state.showContactOpen} onClose = {(e) => this.setState({showContactOpen: false})}/>
+
+               {this.state.showContactOpen ? <EditContactModal id="EditContactss" isEditContactOpen = {this.state.showContactOpen} onClose = {(e) => this.setState({showContactOpen: false})}>
+               <Card className = "CurrentContactCard">
+                  <CardBody>
+                     <h4>Edit Contact</h4>
+                     <form className = "EditContact" id={this.state.editPreFillLastName}>
+                        <label id = "first-name">First Name</label>
+                        <input placeholder={this.state.editPreFillFirstName} type = "text" id = "first-name-edit" name = "first-name"/>
+
+                        <label id = "last-name">Last Name</label>
+                        <input placeholder={this.state.editPreFillLastName} type = "text" id = "last-name" name = "last-name"/>
+
+                        <label id = "company">Company</label>
+                        <input placeholder={this.state.editPreFillCompany} type = "text" id = "company" name = "company"/>
+
+                        <label id = "phone-number">Phone Number</label>
+                        <input placeholder={this.state.editPreFillPhone} type = "text" id = "phone-number" name = "phone-number"/>
+
+                        <label id = "email">Email</label>
+                        <input placeholder={this.state.editPreFillEmail} type = "text" id = "email" name = "email"/>
+
+                        <label id = "address">Address</label>
+                        <input placeholder={this.state.editPreFillAddress} type = "text" id = "address" name = "address"/>
+
+                        <br></br>
+
+                        <button class = "EditButton" onClick = {this.submitEditContact}>Edit</button>
+                        <button onClick = {(e) => this.setState({showContactOpen: false})}>Close</button>
+                     </form>
+                  </CardBody>
+               </Card>
+               </EditContactModal> : null}
+
+
                <br></br>
                <h1 id='title'>React Dynamic Table</h1>
                <table id='students'>
